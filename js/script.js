1cdd60f99b4f84e0fda8956987ec1fad621f0d0b
freelancerslava1985@mail.ru <freelancerslava1985@mail.ru>
2019-12-09 22:17:34 +0300

    lesson_13

--- js/script.js
+++ js/script.js
@@ -93,4 +93,49 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 
+
+    //form
+
+    let message = {
+        loading: 'загрузка...',
+        success: 'спасибо! скоро мы с вами свяжемся!',
+        failure: 'что то пошло не так...'
+    };
+    let form = document.querySelector('main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader ('content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj [key] = value;   
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readestatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            }else if(request.readyState ===4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            }else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
 });
